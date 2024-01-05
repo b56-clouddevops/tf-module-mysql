8eceed9c5995c9af2004402a8e05bc1bded4227a
CodingManoj <CodingManoj@gmail.com>
2024-01-05 07:05:40 +0530

    TF VPC Auto Push

--- rds.tf
+++ rds.tf
@@ -1,31 +1,29 @@
-# Provisions Document DB Cluster
-resource "aws_docdb_cluster" "docdb" {
-  cluster_identifier      = "roboshop-${var.ENV}-docdb"
-  engine                  = "docdb"
-  master_username         = "admin1"
-  master_password         = "roboshop1"
-#   backup_retention_period = 5                             # Commented to avoid backups in lab
-#   preferred_backup_window = "07:00-09:00"
-  skip_final_snapshot     = true
-  db_subnet_group_name    = aws_docdb_subnet_group.docdb.name
-  vpc_security_group_ids  = [aws_security_group.allows_docdb.id]
+# Provisions RDS Instance
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "roboshop-${var.ENV}-mysql"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "admin1"
+  password             = "RoboShop1"
+  parameter_group_name = aws_db_parameter_group.mysql.name
+  skip_final_snapshot  = true
+  db_subnet_group_name = aws_db_subnet_group.mysql.name
 }
 
+# Provisions RDS Parameter Group
+resource "aws_db_parameter_group" "mysql" {
+  name   = "roboshop-${var.ENV}-mysql-pg"
+  family = "mysql5.7"
+}
 
-# Creates a subnet-groups
-resource "aws_docdb_subnet_group" "docdb" {
-  name       = "roboshop-${var.ENV}-docdb-subnet-group"
+# # Creates a subnet-groups
+resource "aws_db_subnet_group" "mysql" {
+  name       = "roboshop-${var.ENV}-mysql-subnet-group"
   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
 
   tags = {
-    Name = "roboshop-${var.ENV}-docdb-subnet-group"
+    Name = "roboshop-${var.ENV}-mysql-subnet-group"
   }
-}
-
-# Creates DocDB Cluster Instances 
-resource "aws_docdb_cluster_instance" "cluster_instances" {
-  count              = 1
-  identifier         = "roboshop-${var.ENV}-docdb"
-  cluster_identifier = aws_docdb_cluster.docdb.id
-  instance_class     = "db.t3.medium"
 }
